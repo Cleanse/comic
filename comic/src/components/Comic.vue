dc61268c3cff579a41bb0fc4ab74fd7debbc1539
Paul E. Lovato <plovato@gmail.com>
2019-06-17 10:59:17 -0600

    New API. Using v-if to check prev/next. Style update.

--- comic/src/components/Comic.vue
+++ comic/src/components/Comic.vue
@@ -1,24 +1,28 @@
 <template>
     <div class="comic-wrapper">
         <div ref="bookblock" class="bb-bookblock" v-bookblock="options">
-            <div class="bb-item" v-for="item in data" :key="item.id">
+            <div class="bb-item" v-for="item in episode.pages" :key="item.id">
                 <div class="page">
-                    <img src="../assets/av-sample.png" alt="Image Title">
-                    <div class="bb-div">{{ item.text[0] }}</div>
+                    <img v-bind:src="item.images[0]" alt="Image Title">
+                    <div class="bb-div">
+                        <a class="bb-nav-prev" href="javascript:" @click="bookblock.prev()">Previous</a>
+                    </div>
                 </div>
 
                 <div class="page">
-                    <img src="../assets/av-sample.png" alt="Image Title 2">
-                    <div class="bb-div">{{ item.text[1] }}</div>
+                    <img v-if="item.images[1]" v-bind:src="item.images[1]" alt="Image Title 2">
+                    <div class="bb-div">
+                        <a class="bb-nav-next" href="javascript:" @click="bookblock.next()">Next</a>
+                    </div>
                 </div>
             </div>
         </div>
 
         <nav>
+            <a class="bb-nav-first" v-if="episode.links.prev" v-bind:href="episode.links.prev">Previous Episode</a>
             <a class="bb-nav-first" href="javascript:" @click="bookblock.first()">First page</a>
-            <a class="bb-nav-prev" href="javascript:" @click="bookblock.prev()">Previous</a>
-            <a class="bb-nav-next" href="javascript:" @click="bookblock.next()">Next</a>
             <a class="bb-nav-last" href="javascript:" @click="bookblock.last()">Last page</a>
+            <a class="bb-nav-last" v-if="episode.links.next" v-bind:href="episode.links.next">Next Episode</a>
         </nav>
     </div>
 </template>
@@ -30,7 +34,7 @@
 
     .bb-bookblock {
         width: 904px;
-        height: 640px;
+        height: 656px;
     }
 
     .bb-item {
@@ -41,13 +45,55 @@
         width: 50%;
         float: left;
         height: 100%;
+        position: relative;
         box-sizing: border-box;
         background: #000;
         border: 1px solid #eee;
 
         .bb-div {
-            margin-top: -2rem;
-            background: #efefef;
+            height: 100%;
+            width: 100%;
+            position: absolute;
+            bottom: 0;
+
+            background-image: linear-gradient(to right, rgba(255, 255, 255, .1) 95%, #DADADA 100%);
+            -webkit-box-shadow: inset 0 0 5px #666;
+            -moz-box-shadow: inset 0 0 5px #666;
+            -o-box-shadow: inset 0 0 5px #666;
+            -ms-box-shadow: inset 0 0 5px #666;
+            box-shadow: inset 0 0 5px #666;
+
+            .bb-nav-prev,
+            .bb-nav-next {
+                height: 100%;
+                width: 100%;
+                padding: 0;
+                margin: 0;
+                opacity: 0;
+                display: block;
+
+                &:hover {
+                    opacity: 1;
+                }
+            }
+
+            .bb-nav-prev, .bb-nav-next {
+                color: #666;
+                font-size: .5rem;
+                font-style: italic;
+                text-decoration: none;
+                line-height: 1.5rem;
+            }
+        }
+
+        &:nth-child(2) .bb-div {
+            right: 0;
+            background-image: linear-gradient(to left, rgba(255, 255, 255, .1) 95%, #DADADA 100%);
+            -webkit-box-shadow: inset 0 0 5px #666;
+            -moz-box-shadow: inset 0 0 5px #666;
+            -o-box-shadow: inset 0 0 5px #666;
+            -ms-box-shadow: inset 0 0 5px #666;
+            box-shadow: inset 0 0 5px #666;
         }
     }
 
@@ -60,19 +106,11 @@
         height: auto;
     }
 
-    .cover {
-        width: 50%;
-        margin-left: auto;
-        float: none;
-    }
-
     nav {
         margin-top: 1em;
     }
 
     .bb-nav-first,
-    .bb-nav-prev,
-    .bb-nav-next,
     .bb-nav-last {
         display: inline-block;
         font-weight: 400;
@@ -92,7 +130,7 @@
 
 <!--https://codepen.io/jasesmith/pen/GmQbME-->
 <script>
-    import {data} from "../data/data.js";
+    import { data } from "../data/data.js";
 
     export default {
         data: function () {
@@ -104,7 +142,7 @@
                     orientation: 'vertical',
                     hasCover: false
                 },
-                data: data
+                episode: data
             };
         },
         computed: {
--- comic/src/data/data.js
+++ comic/src/data/data.js
@@ -1,20 +1,26 @@
-export const data = [
-    {
-        text: {
-            0: 'This is page one.',
-            1: 'This is page two.'
+export const data = {
+    "pages": [
+        {
+            images: {
+                0: "./assets/images/7-1-1.png",
+                1: "./assets/images/7-1-2.png"
+            }
+        },
+        {
+            images: {
+                0: "./assets/images/7-1-3.png",
+                1: "./assets/images/7-1-4.png"
+            }
+        },
+        {
+            images: {
+                0: "./assets/images/7-1-5.png",
+                1: "./assets/images/blank.png"
+            }
         }
+    ],
+    "links": {
+        "prev": "/episode/1",
+        "next": "/episode/2"
     },
-    {
-        text: {
-            0: 'This is page three.',
-            1: 'This is page four.'
-        }
-    },
-    {
-        text: {
-            0: 'This is page five.',
-            1: 'This is page six.'
-        }
-    }
-];
+};
